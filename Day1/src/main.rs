4d2b2ecfd95be9b95cdc16564a3f5bb586fe7e37
Sam Miller <sithpow@gmail.com>
2023-01-20 19:57:54 -0600

    day1 part 1 successful

--- Day1/src/main.rs
+++ Day1/src/main.rs
@@ -13,11 +13,24 @@ fn read<R: Read>(io: R) -> Result<Vec<i64>, Error> {
 
 fn main() -> Result<(), Error> {
     let vec = read(File::open("./input")?)?;
-    let max::i64 = 0;
-    for _item in _vec{
-        
-        
+    let mut max: i64 = 0;
+    let mut sum: i64 = 0;
+    for _item in vec{
+        if _item == 0{
+            if sum > max{
+                max = sum;
+                sum = 0;
+            }
+            else{
+                sum=0;
+            }
+        }
+        else{
+            sum = sum + _item;
+
+        }
 
     }
+    print!("{:?}",max);
     Ok(())
 }
